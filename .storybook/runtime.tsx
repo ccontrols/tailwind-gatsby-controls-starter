0fcbea4d8194d816bb4c11386b7aa09a84f27477
atanasster <atanasster@gmail.com>
2020-11-26 03:21:34 -0500

    initial check-in

---
+++ .storybook/runtime.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { RunConfiguration, defaultRunConfig } from "@component-controls/core";
+import { TestingPage } from "./testing-page";
+import '../src/tailwind.output.css';
+
+const config: RunConfiguration = {
+  analytics: 'UA-XXXXXXXXX-X',
+  title: `Tailwind component-controls`,
+  description: `Tailwind project with typescript, react testing library and component-controls`,
+  author: `@component-controls`,
+  pages: {
+    story: {
+      tabs: [
+        ...defaultRunConfig.pages.story.tabs,
+        { title: 'Testing', render: () => <TestingPage /> },
+      ],
+    },
+  }  
+};
+export default config;
