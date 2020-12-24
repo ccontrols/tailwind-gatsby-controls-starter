817d3dfe00e08101fa59696e84d1fdc5869fdaae
atanasster <atanasster@gmail.com>
2020-12-24 01:16:02 -0500

    upgrade packages

--- .storybook/runtime.tsx
+++ .storybook/runtime.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { RunConfiguration, defaultRunConfig } from "@component-controls/core";
-import { TestingPage } from "./testing-page";
+import { RunConfiguration } from '@component-controls/core';
 import '../src/tailwind.output.css';
 
 const config: RunConfiguration = {
@@ -8,13 +7,6 @@ const config: RunConfiguration = {
   title: `Tailwind component-controls`,
   description: `Tailwind project with typescript, react testing library and component-controls`,
   author: `@component-controls`,
-  pages: {
-    story: {
-      tabs: [
-        ...defaultRunConfig.pages.story.tabs,
-        { title: 'Testing', render: () => <TestingPage /> },
-      ],
-    },
-  }  
 };
+
 export default config;
